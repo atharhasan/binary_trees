b4669fa68169359cc75139cc561aa46aaa8ab71d
atharhasan <athar_h12@yahoo.com>
2024-02-28 12:08:39 +0200

    commit task

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,6 +4,7 @@
  * binary_tree_node - creates a binary tree node.
  * @parent: is a pointer to the parent node of the node to create
  * @value: is the value to put in the new node.
+ * Return: the pointer of new node.
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
@@ -11,7 +12,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
