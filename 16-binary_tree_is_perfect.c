418ae1eba135a7297d17f600471f7ed4d5b5637b
atharhasan <athar_h12@yahoo.com>
2024-02-28 23:54:25 +0200

    commit task

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,26 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
+ * binary_tree_leaves - Counting the leaves of a binary tree
  * @tree: a pointer to the root node of the tree to traverse
  *
- * Return: the higth of a binary tree.
+ * Return: the count of a binary tree leaves.
  */
 
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t right_height = 0, left_height = 0;
+	size_t right_leaves = 0, left_leaves = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left)
-		left_height = binary_tree_height(tree->left) + 1;
+		left_leaves = binary_tree_leaves(tree->left);
 
 	if (tree->right)
-		right_height = binary_tree_height(tree->right) + 1;
+		right_leaves = binary_tree_leaves(tree->right);
 
-	return (left_height > right_height ? left_height : right_height);
+	return ((!right_leaves && !left_leaves) ? 1 : left_leaves + right_leaves);
 }
 
 /**
@@ -33,16 +33,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-		size_t right_side = 0, left_side = 0;
+		size_t right_side = 0, left_side = 0, left_leaves = 0,
+		right_leaves = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left)
-		left_side = binary_tree_height(tree->left);
-
+	{
+		left_side = binary_tree_is_perfect(tree->left);
+		left_leaves = binary_tree_leaves(tree->left);
+	}
 	if (tree->right)
-		right_side = binary_tree_height(tree->right);
+	{
+		right_side = binary_tree_is_perfect(tree->right);
+		right_leaves = binary_tree_leaves(tree->right);
+	}
 
-	return ((left_side == right_side) ? 1 : 0);
+	return ((left_side == right_side) && (left_leaves == right_leaves) ? 1 : 0);
 }
