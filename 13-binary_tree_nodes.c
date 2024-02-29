c5b42e3b0861b33f9366f13e87abbbd92cffdf9e
Mohammad <mohammadelkhiat@gmail.com>
2024-02-29 02:20:14 +0200

    0_0

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - Counting the leaves of a binary tree
+ * binary_tree_nodes - Counting the nodes with at least 1 child in a binary tree
  * @tree: a pointer to the root node of the tree to traverse
  *
- * Return: the count of a binary tree leaves.
+ * Return: the count of nodes with at least 1 child.
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
