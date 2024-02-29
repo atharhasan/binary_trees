e2ebc892d502f8bc6493e2ad9a6cbf9bebadb646
Mohammad <mohammadelkhiat@gmail.com>
2024-02-29 02:57:37 +0200

    0_0

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -24,7 +24,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_ful - a function that checks if a binary tree is full
+ * binary_tree_is_full - a function that checks if a binary tree is full
  * @tree: a pointer to the root node of the tree to traverse
  *
  * Return: 1 if the binary tree is full, 0 if not OR...
